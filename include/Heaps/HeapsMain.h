a80e2b57dd2b61b782b50c4fcf3ed3f26e2a7a5c
Jlisowskyy <Jlisowskyy@gmail.com>
2023-11-14 21:37:25 +0100

    heaps added

---
+++ include/Heaps/HeapsMain.h
@@ -0,0 +1,15 @@
+//
+// Created by Jlisowskyy on 11/8/23.
+//
+
+#ifndef MINI_PROJECTS_HEAPSMAIN_H
+#define MINI_PROJECTS_HEAPSMAIN_H
+
+#include "Beap.h"
+
+int HeapsMain()
+{
+
+}
+
+#endif //MINI_PROJECTS_HEAPSMAIN_H
--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 // Author: Jakub Lisowski
 #include "include/btree/btreeMain.h"
 #include "include/hanoi/hanoiMain.h"
+#include "include/Heaps/HeapsMain.h"
 
 const bool displayBtree = false;
-const bool displayHanoi = true;
+const bool displayHanoi = false;
+const bool displayHeaps = true;
 
 int main()
 {
     if constexpr (displayBtree) BtreeMain();
     if constexpr (displayHanoi) HanoirMain();
+    if constexpr (displayHeaps) HeapsMain();
 
     return 0;
 }
