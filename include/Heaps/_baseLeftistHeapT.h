b58ed015964e954e38006f6d7287981e11f6f0f5
Jlisowskyy <Jlisowskyy@gmail.com>
2023-12-02 13:26:28 +0100

    leftist heap close to done

--- include/Heaps/_baseLeftistHeapT.h
+++ include/Heaps/_baseLeftistHeapT.h
@@ -67,12 +67,32 @@ public:
     // Type interaction
     // ------------------------------
 
+    static void ChangePrintRecuOffsetStep(const unsigned int newStep) {
+        PrintRecuOffsetStep = newStep;
+        SpacingStep = std::string(newStep, '\n');
+    }
+
+    static void ChangePrintLevelSpacing(const unsigned int newSize) {
+        PrintSpaceDist = newSize;
+        SpacingString = std::string(newSize, '\n');
+    }
+
     _baseLeftistHeapT& Insert(const PrioT& prio, const ItemT& item)
     {
-        _insert(prio, item);
+        auto n = new node{prio, item};
+        _insert(n);
         ++_elemCounter;
         return *this;
     }
+
+    // TODO
+    // _baseLeftistHeapT& Insert(mPair)
+    // {
+    //     auto n = new node{prio, item};
+    //     _insert(n);
+    //     ++_elemCounter;
+    //     return *this;
+    // }
 
     _baseLeftistHeapT& Max(mPair& out) const
         // when tree is empty behaviour is undefined
@@ -163,8 +183,55 @@ public:
         return ret;
     }
 
+    bool Delete(const PrioT prio, mPair& out) {
+        node** res = _search(&_root, prio);
+        if (res == nullptr) return false;
+
+        out = (*res)->content;
+        _delete(res);
+
+        return true;
+    }
+
+    bool Delete(const PrioT prio)
+    {
+        node** res = _search(&_root, prio);
+        if (res == nullptr) return false;
+
+        _delete(res);
+
+        return true;
+    }
+
+    bool Replace(const PrioT prio, const mPair& newItem, mPair& oItem) {
+        node** res = _search(&_root, prio);
+        if (res == nullptr) return false;
+
+        oItem = (*res)->content;
+        auto n = new node{newItem};
+        _replace(res, n);
+
+        return true;
+    }
+
+    bool Replace(const PrioT prio, const mPair& newItem) {
+        node** res = _search(&_root, prio);
+        if (res == nullptr) return false;
+
+        auto n = new node{newItem};
+        _replace(res, n);
+
+        return true;
+    }
+
+    // TODO: UPDATE KEY
+    // template<class PriorityUpdateFunction>
+
     std::ostream& operator<<(std::ostream& out, const _baseLeftistHeapT& tree) {
-        tree._print(out);
+        auto printParams = tree._findPrintParams();
+        out << std::setw(printParams.second);
+
+        _printRecu(out, _root, 0);
         return out;
     }
 
@@ -173,28 +240,69 @@ public:
     // -------------------------------
 private:
 
-    static size_t _findMaxPrint(node* nd) {
-        if (nd == nullptr) return 0;
+    void _delete(node** tree) {
+        node* nSubTree = _merge((*tree)->left, (*tree)->right);
+        delete *tree;
+        *tree = nSubTree;
+    }
+
+    void _replace(node** tree, node** n) {
+        node* nSubTree = _merge((*tree)->left, n);
+        nSubTree = _merge((*tree)->right, nSubTree);
+        delete *tree;
+        *tree = nSubTree;
+    }
+
+    static void _printRecu(std::ostream& out, const node* tree, const size_t offset) {
+        static auto PrintContent = [&] {
+            for(int i = 0; i < offset; ++i)
+                out << SpacingStep;
+            out << *tree;
+        };
+
+        if (!tree) return;
+
+        _printRecu(out, tree->left, offset+1);
+        PrintContent();
+        _printRecu(out, tree->right, offset+1);
+    }
+
+    void _print(std::ostream& out) const{
+        // TODO: Make nice looking printing procedure
+        (void)_root;
+        out << "Not implemented\n";
+    }
+
+    // Returns height of the tree as first and maximal length of printed priority as second
+    static std::pair<size_t, size_t> _findPrintParams(node* nd)
+    {
+        if (nd == nullptr) return {0, 0};
 
         std::ostringstream str{};
         str << *nd;
         size_t max = str.str().length();
+        const auto [lHeight, lMax] = _findMaxPrint(nd->left);
+        const auto [rHeight, rMax] = _findMaxPrint(nd->right);
 
-        if (const size_t lRes = _findMaxPrint(nd->left); lRes > max) max = lRes;
-        if (const size_t rRes = _findMaxPrint(nd->right); rRes > max) max = rRes;
+        if (lMax > max) max = lMax;
+        if (rMax > max) max = rMax;
 
-        return max;
+        return {lHeight > rHeight ? lHeight + 1 : rHeight + 1, max};
     }
 
-    void _print(std::ostream& out) const{
+    static node** _search(const node** n, PrioT prio)
+        // when n is a nullptr behaviour is undefined
+    {
+        if (!*n) return nullptr;
 
+        if (_pred(prio, (*n)->content.first)) return nullptr;
+        if (_pred((*n)->content.first, prio)) {
+            node* res = _search(&(*n)->left, prio);
 
-    }
-
-    static node** _search(const node** n, PrioT prio) {
-
-
-        return nullptr;
+            if (res == nullptr) res = _search(&(*n)->right, prio);
+            return res;
+        }
+        return n; // found
     }
 
     // TODO: CAN BE A LOT FASTER
@@ -211,8 +319,7 @@ private:
         return n; // found
     }
 
-    void _insert(const PrioT& prio, const ItemT& item) {
-        node* n{prio, item};
+    void _insert(node* n) {
         _root = _merge(_root, n);
     }
 
@@ -292,8 +399,10 @@ private:
 
     struct node {
         node() = default;
-        node(const PrioT nPrio, const ItemT nItem, const size_t nNpl = 0, node* nLeft = nullptr, node* nRight = nullptr):
+        node(const PrioT& nPrio, const ItemT& nItem, const size_t nNpl = 0, node* nLeft = nullptr, node* nRight = nullptr):
             content{ nPrio, nItem }, left{ nLeft }, right{ nRight }, npl{ nNpl }{}
+        explicit node(const mPair& pair, const size_t nNpl = 0, node* nLeft = nullptr, node* nRight = nullptr):
+            content{ pair }, left{ nLeft }, right{ nRight }, npl{ nNpl }{}
 
         std::pair<PrioT, ItemT> content;
         node* left;
@@ -333,6 +442,11 @@ private:
     node* _root = nullptr;
     size_t _elemCounter = 0;
     static PriorityFunction _pred;
+
+    inline static unsigned int PrintRecuOffsetStep = 5;
+    inline static std::string SpacingStep = std::string(PrintRecuOffsetStep, ' ');
+    inline static unsigned int PrintSpaceDist = 3;
+    inline static std::string SpacingString = std::string(PrintSpaceDist, '\n');
 };
 
 #endif //_BASELEFTISTHEAPT_H
